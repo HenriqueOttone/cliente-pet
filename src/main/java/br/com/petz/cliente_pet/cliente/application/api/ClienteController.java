81fa368f1e06e7df1ec9e35930106773927c2ccc
HenriqueOttone <ottonehenrique@hotmail.com>
2026-07-20 19:39:51 -0300

    imnplementa controller

---
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteController.java
@@ -0,0 +1,21 @@
+package br.com.petz.cliente_pet.cliente.application.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.petz.cliente_pet.cliente.domain.Cliente;
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@Log4j2
+public class ClienteController implements ClienteAPI {
+
+	@Override
+	public ClienteResponse postCliente(ClienteRequest clienteRequest) {
+
+		log.info("[inicia] ClienteController -  postCliente");
+		log.info("[finaliza] ClienteController -  postCliente");
+		
+		return null;
+	}
+
+}
